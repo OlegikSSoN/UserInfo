76e402492a87bdc27d2b777108c7fc2d857251be
OlegikSSoN <olegiksson@gmail.com>
2023-02-03 19:51:15 +0200

    cardinfo

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const card = prompt("Введіть номер картки");
 const name = prompt("Ваше ім'я");
 const country = prompt("Країна проживання USA, EN, UK");
-const age = Number(prompt("Ваш рік народження"));
-const userdate = Number(new Date().getFullYear());
-const userage = Number(userdate - age);
+const age = Number(prompt("Ваш вік"));
 
-if ((country == "USA" || country == "EN") && age >= 18) {
+if ((country == "USA", "EN" && age >= 18)) {
   alert(
-    `Good afternoon ${name}, your year of birth is: ${userage}. Funds will be to your card: ${card}. By place of residence: ${country}`
+    `Good afternoon ${name}, your year of birth is: ${
+      2023 - age
+    }. Funds will be to your card: ${card}. By place of residence: ${country}`
   );
 }
 
 if (country == "UK" && age >= 18) {
   alert(
-    `Доброго дня ${name}, Ваш рік народження: ${userage}. Вам буде надіслано кошти на картку: ${card}. По місцю проживання: ${country}`
+    `Доброго дня ${name}, Ваш рік народження: ${
+      2023 - age
+    }. Вам буде надіслано кошти на картку: ${card}. По місцю проживання: ${country}`
   );
+} else {
+  alert("Ви занадто молоді! / You are too young!");
 }
